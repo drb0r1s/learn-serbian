4604f0137d6473ec674bec77d7ec9edf0f2f8db1
Boris <drb0r1sdev@gmail.com>
2024-07-23 20:09:20 +0200

    Lessons page updates

--- src/pages/lessons/Lessons.jsx
+++ src/pages/lessons/Lessons.jsx
@@ -1,34 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Header from "../../components/header/Header";
 import LessonsTree from "./LessonsTree";
 import { images } from "../../data/images";
 
 const Lessons = () => {
-    const parent = useRef(null);
-    const child = useRef(null);
-
-    useEffect(() => {
-        child.current.addEventListener("wheel", doScroll, { passive: true });
-    }, []);
-
-    function doScroll(event) {
-        const scrollSpeed = 500;
-        const scrollValue = parent.current.scrollTop + (event.deltaY > 0 ? scrollSpeed : scrollSpeed * -1);
-        
-        parent.current.scrollBy({
-            top: scrollValue,
-            behavior: "smooth"
-        });
-    }
-    
     return(
         <section id="lessons">
             <Header />
             
-            <div className="lessons-holder" ref={parent}>
-                <div className="lessons-info-holder-spacer"></div>
-
-                <div className="lessons-info-holder" ref={child}>
+            <div className="lessons-holder">
+                <div className="lessons-info-holder">
                     <h2>Lesson title</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet possimus cum nam minus tenetur unde inventore ipsum sunt, temporibus harum eius quam ipsam, eos tempora nihil tempore? Minus, placeat esse!</p>
                 

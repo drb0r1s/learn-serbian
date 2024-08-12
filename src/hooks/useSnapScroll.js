10455773956e3dcf291ab520d702fb3c01779845
Boris <drb0r1sdev@gmail.com>
2024-08-12 16:03:28 +0200

    useSnapScroll() hook updates

--- src/hooks/useSnapScroll.js
+++ src/hooks/useSnapScroll.js
@@ -1,25 +1,50 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const useSnapScroll = (element, dependency = []) => {
+    const touchStartY = useRef(0);
+    
     useEffect(() => {
         if(!element) return;
         
-        element.addEventListener("wheel", snapScroll, { passive: false });
-        element.addEventListener("touchmove", snapScroll, { passive: false });
-        element.addEventListener("keydown", snapScroll, { passive: false });
+        element.addEventListener("wheel", wheelScrolling, { passive: false });
+        element.addEventListener("touchstart", touchStartScrolling, { passive: false });
+        element.addEventListener("touchmove", touchMoveScrolling, { passive: false });
+        element.addEventListener("keydown", keydownScrolling, { passive: false });
 
         return () => {
-            element.removeEventListener("wheel", snapScroll, { passive: false });
-            element.removeEventListener("touchmove", snapScroll, { passive: false });
-            element.removeEventListener("keydown", snapScroll, { passive: false });
+            element.removeEventListener("wheel", wheelScrolling, { passive: false });
+            element.removeEventListener("touchstart", touchStartScrolling, { passive: false });
+            element.removeEventListener("touchmove", touchMoveScrolling, { passive: false });
+            element.removeEventListener("keydown", keydownScrolling, { passive: false });
         }
     }, [element, ...dependency]);
 
-    function snapScroll(e) {
+    function wheelScrolling(e) {
         e.preventDefault();
         
         const top = e.deltaY > 0 ? window.innerHeight : window.innerHeight * -1;
         element.scrollBy({ top, behavior: "smooth" });
+    }
+
+    function touchStartScrolling(e) {
+        e.preventDefault();
+        touchStartY.current = e.touches[0].clientY;
+    }
+    
+    function touchMoveScrolling(e) {
+        e.preventDefault();
+        
+        const touchMoveY = e.touches[0].clientY;
+
+        const top = touchMoveY < touchStartY.current ? window.innerHeight : window.innerHeight * -1;
+        element.scrollBy({ top, behavior: "smooth" });
+
+        touchStartY.current = touchMoveY;
+    }
+
+    function keydownScrolling(e) {
+        e.preventDefault();
+        console.log(e)
     }
 }
 

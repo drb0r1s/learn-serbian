f1a87e4b02e89ab6b454374f5794e999fe123a0e
Boris <drb0r1sdev@gmail.com>
2024-08-24 20:42:27 +0200

    InnerConversation updates

--- src/pages/lessons/inner/content/InnerConversation.jsx
+++ src/pages/lessons/inner/content/InnerConversation.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useContent from "../../../../hooks/useContent";
 import { images } from "../../../../data/images";
+import { Language } from "../../../../functions/Language";
 
 const InnerConversation = ({ block, blockJump }) => {
     const buttonContent = useContent("lessonsInner.button_conversation_continue");
@@ -8,12 +9,12 @@ const InnerConversation = ({ block, blockJump }) => {
     return(
         <div className="lessons-inner-block lessons-inner-conversation">
             <div className="lessons-inner-block-holder lessons-inner-conversation-holder">
-                <h3>{block.title}</h3>
+                <h3>{Language.inject(block.title)}</h3>
                 
                 <div className="lessons-inner-conversation-display">
                     <div className="lessons-inner-conversation-display-user-holder">
-                        <img src={images.noAvatar} alt={block.conversationWith} />
-                        <strong>{block.conversationWith}</strong>
+                        <img src={images.noAvatar} alt={Language.inject(block.conversationWith)} />
+                        <strong>{Language.inject(block.conversationWith)}</strong>
                     </div>
 
                     <div className="lessons-inner-conversation-display-messages-holder">
@@ -21,11 +22,12 @@ const InnerConversation = ({ block, blockJump }) => {
                     </div>
 
                     <div className="lessons-inner-conversation-display-keyboard-holder">
-                        
+                        <input type="text" placeholder="" />
+                        <button><img src={images.playIcon} alt="SEND" /></button>
                     </div>
                 </div>
                 
-                <button>{buttonContent}</button>
+                <button className="disabled-button">{buttonContent}</button>
             </div>
         </div>
     );
